89ba71a55b2515b7d752ea450342776db3269a9b
Mariya Lysenkova <mariya@rampageconsulting.com>
2018-11-05 14:06:55 +0100

    Simple set list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Set from './Set.js';
+
+const REBRICKABLE_API_KEY='0b6a4abaa97580f8f57ec86abe790907'
 
 class App extends Component {
+  state = { 
+    sets: [],
+  }
+
+  componentDidMount() {
+    this.fetchSets();
+  }
+
+  fetchSets = () => {
+    fetch('https://rebrickable.com/api/v3/lego/sets/', {
+      headers: {
+        'Authorization': 'key ' + REBRICKABLE_API_KEY
+      }})
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        this.setState({ sets: json.results });
+      });
+  }
+
   render() {
     return (
       <div className="App">
+        <ul>
+        {
+          this.state.sets.map(function(set, i){
+            return <Set set={set} key={i} />;
+        })}
+        </ul>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
