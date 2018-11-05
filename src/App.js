4fc92fcb178ba7ba7845855a201716140432bf7d
Mariya Lysenkova <mariya@rampageconsulting.com>
2018-11-05 17:47:44 +0100

    Styles and messages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import './App.css';
 import Set from './Set.js';
 
 const REBRICKABLE_API_KEY='0b6a4abaa97580f8f57ec86abe790907'
+const MIN_SETS=30
+
+class RandomButton extends Component {
+  render() {
+    return (
+      <button onClick={this.props.fetch}>
+        Gimme New Lego
+      </button>
+    );
+  }
+}
 
 class App extends Component {
   state = { 
     sets: [],
     randomTheme: '',
+    finding: true,
   }
 
   componentDidMount() {
@@ -24,7 +36,9 @@ class App extends Component {
         const randomIndex = Math.floor(Math.random() * Math.floor(json.results.length - 1));
         const randomTheme = json.results[randomIndex];
         this.setState({ 
-          randomTheme: randomTheme
+          sets: [],
+          randomTheme: randomTheme,
+          finding: true,
         });
         this.fetchSets(randomTheme.id);
       });
@@ -37,17 +51,37 @@ class App extends Component {
       }})
       .then(res => res.json())
       .then(json => {
+        console.log(json)
+        if (json.results.length < MIN_SETS) {
+          return this.fetchRandomTheme();
+        }
         this.setState({ 
           sets: json.results,
-          themeIds: json.results.map(set => set.theme_id)
+          themeIds: json.results.map(set => set.theme_id),
+          finding: false
         });
       });
+  }
+
+  statusMessage = () => {
+    if (this.state.finding) {
+      return <div className="status">Hold tight, finding you some random lego sets...</div>;
+    } else {
+      return <div className="status">We found you this random-ass lego:</div>;
+    }
   }
 
   render() {
     return (
       <div className="App">
-      <h1>{this.state.randomTheme.name}</h1>
+        <div className="banner">Your one-stop shop for Christmas Lego for your colleagues at SNP&SEQ</div>
+        <RandomButton fetch={this.fetchRandomTheme}></RandomButton>
+        <div className="logo">
+          <img className="dice" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Dice_simple.svg/2000px-Dice_simple.svg.png" />
+          <h1>Bricker</h1>
+        </div>  
+        {this.statusMessage()}
+        <h2>{this.state.randomTheme.name}</h2>
         <ul>
         {
           this.state.sets.map(function(set, i){
